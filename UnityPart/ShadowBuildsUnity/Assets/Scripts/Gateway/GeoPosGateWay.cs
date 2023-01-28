8f47fc72a697991777d91f7ec4534d579823838c
Boriswp <59508655+Boriswp@users.noreply.github.com>
2023-01-28 13:54:44 +0500

    add RestClient

---
+++ UnityPart/ShadowBuildsUnity/Assets/Scripts/Gateway/GeoPosGateWay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoPosGateWay : MonoBehaviour
+{
+
+    private readonly string basePath = "http://192.168.134.187:5000";
+    private RequestHelper currentRequest;
+
+    public static GeoPosGateWay Instance;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
